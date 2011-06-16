08fa080f0aebe94771d63f6dbfea723ccfb35086
Gregory Eric Sanderson <gzou2000@gmail.com>
2011-06-15 22:32:22 -0400

    task commands now use get_resource

--- tiktok/commands/task.py
+++ tiktok/commands/task.py
@@ -1,30 +1,30 @@
 from tiktok.model.task import Task
-from tiktok.lib.resources import resource
+from tiktok.lib.resources import get_resource
 import pprint
 
 def show( args ):
 
-    task = Task( resource )
+    task = Task( get_resource() )
     task_id = int(args[0])
     pprint.pprint( task.show( task_id ) )
 
 
 def start( args ):
 
-    task = Task( resource )
+    task = Task( get_resource() )
     task_id = int(args[0])
     task.start( task_id )
 
 
 def stop( args ):
 
-    task = Task( resource )
+    task = Task( get_resource() )
     task_id = int(args[0])
     task.stop( task_id )
 
 
 def current( args ):
 
-    task = Task( resource )
+    task = Task( get_resource() )
     pprint.pprint( task.current() )
 
