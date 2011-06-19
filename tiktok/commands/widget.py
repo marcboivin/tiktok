87dbc8ed1ae884123c84376c7a3939f04503d1aa
Gregory Eric Sanderson <gzou2000@gmail.com>
2011-06-19 16:05:45 -0400

    rewrote widget commands with new Widget model

--- tiktok/commands/widget.py
+++ tiktok/commands/widget.py
@@ -1,22 +1,17 @@
 from tiktok.model.widget import Widget
 from tiktok.model.task import Task
 
-from tiktok.lib.resources import get_resource
-
 import pprint
 import sys
 
 def tasks( args ):
 
-    widget = Widget( get_resource() )
-    tasks = widget.tasks( args['widget_id'] )
+    tasks = Widget.tasks( args['widget_id'] )
     for task in tasks:
-        print Task.format( task )
+        print task.format()
 
 def list( args ):
 
-    widget = Widget( get_resource() )
-    widgets = widget.list()
-    for widget in widgets:
-       print Widget.format( widget )
+    for widget in Widget.list():
+        print widget.format()
 
