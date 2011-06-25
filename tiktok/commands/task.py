59d2f585c67c581788a2a84f7fc515cb4d68db98
Gregory Eric Sanderson <gzou2000@gmail.com>
2011-06-25 18:22:27 -0400

    refactored task commands

--- tiktok/commands/task.py
+++ tiktok/commands/task.py
@@ -1,28 +1,28 @@
 from tiktok.model.task import Task
 import pprint
 
-def show( args ):
+def show( args, config, **kwargs ):
 
     task = Task.get( args['tasknum'] )
     pprint.pprint( task )
 
-def start( args ):
+def start( args, config, **kwargs ):
 
     task = Task.get( args['tasknum'] )
     task.start()
 
-def updatelog( args ):
+def updatelog( args, config, **kwargs ):
 
     Task.updatelog( args['description'] )
 
-def stop( args ):
+def stop( args, config, **kwargs ):
 
     if 'log' in args:
-        updatelog( {'description' :  args['log']} )
+        updatelog( {'description' :  args['log']}, config )
 
     Task.stop()
 
-def current( args ):
+def current( args, config, **kwargs ):
 
     task = Task.current()
     pprint.pprint( task )
