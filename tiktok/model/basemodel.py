be3a7f1762120b378c78ae5995129adf5c6f9a6e
Gregory Eric Sanderson <gzou2000@gmail.com>
2011-07-02 18:14:22 -0400

    added function for parsing utc ISO datetimes

--- tiktok/model/basemodel.py
+++ tiktok/model/basemodel.py
@@ -1,3 +1,7 @@
+import datetime
+import time
+
+from tiktok.lib.durations import total_seconds
 
 utils = {}
 
@@ -8,6 +12,15 @@ def get_utils():
 def set_utils( new_utils ):
     global utils
     utils = new_utils
+
+utc_datetime = '%Y-%m-%dT%H:%M:%SZ'
+
+def parse_isoutc( text ):
+
+    dt = datetime.datetime.strptime( text, utc_datetime )
+    epoch = datetime.datetime(1970, 1, 1, 0, 0, 0)
+    secs = total_seconds( dt - epoch )
+    return datetime.datetime.utcfromtimestamp( secs )
 
 def resourcemethod( func ):
 
@@ -23,6 +36,7 @@ class BaseModel( dict ):
 
     def __init__(self, attributes, **kwargs ):
 
+        dict.__init__( self )
         for key, value in kwargs.items():
             setattr( self, key, value )
         self.update( attributes )
