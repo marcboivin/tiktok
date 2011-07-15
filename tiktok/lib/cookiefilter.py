1a8469a2b803819ffeccecf84bd14db8324a413c
Gregory Eric Sanderson <gregory@sys-tech.net>
2011-07-15 17:30:28 -0400

    bugfix: capitalized headers on PPA python 2.7.1+
    For some weird reason, headers on mathieu's box
    were returned capitalized, which throws cookielib
    out of whack. Added a function for sanitizing headers

--- tiktok/lib/cookiefilter.py
+++ tiktok/lib/cookiefilter.py
@@ -6,11 +6,19 @@ class InfoWrapper( object ):
     def __init__(self, headers):
         self.headers = headers
 
+    def sanitize_header_names( self, headers ):
+        return dict(
+            ( header.title(), value )
+            for ( header, value ) in headers.items()
+        )
+
     def getheaders(self, name):
-        return [ value for (key, value) in self.headers.items() if key == name ]
+        headers = self.sanitize_header_names( self.headers )
+        return [ value for (key, value) in headers.items() if key == name ]
 
     def getallmatchingheaders(self, name):
-        return [ "%s: %s" % (key, value) for (key, value) in self.headers.items() if key == name ]
+        headers = self.sanitize_header_names( self.headers )
+        return [ "%s: %s" % (key, value) for (key, value) in headers.items() if key == name ]
 
 class ResourceWrapper( object ):
 
@@ -22,7 +30,8 @@ class ResourceWrapper( object ):
 
 class CookieFilter( object ):
 
-    def __init__(self, cookiejar = cookielib.CookieJar()):
+    def __init__(self, cookiejar = None):
+        cookiejar = cookiejar or cookielib.CookieJar()
         self.cookiejar = cookiejar
 
     def on_request(self, request):
@@ -34,6 +43,7 @@ class CookieFilter( object ):
 
     def on_response(self, response, request):
         fake_request = urllib2.Request( url = request.url )
-        fake_resource = ResourceWrapper( response.headers )
+        headers = response.headers
+        fake_resource = ResourceWrapper( headers )
         self.cookiejar.extract_cookies( fake_resource, fake_request )
 
