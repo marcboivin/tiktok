c899190bc33d91fdb7713eeb792b9d49fc0d9bf5
Gregory Eric Sanderson <gzou2000@gmail.com>
2011-07-16 14:34:50 -0400

    refactored widget commands to use helpers

--- tiktok/commands/widget.py
+++ tiktok/commands/widget.py
@@ -4,18 +4,14 @@ from tiktok.model.task import Task
 import pprint
 import sys
 
-def tasks( args, config, **kwargs ):
-
-    printer = kwargs['printer']
+def tasks( args, helpers ):
 
     tasks = Widget.tasks( args['widget_id'] )
     for task in tasks:
-        printer.pprint( task, config['task']['format'] )
+        helpers.pprint( task, helpers.config['task']['format'] )
 
-def list( args, config, **kwargs ):
-    
-    printer = kwargs['printer']
+def list( args, helpers ):
 
     for widget in Widget.list():
-        printer.pprint( widget, config['widget']['format'] )
+        helpers.pprint( widget, helpers.config['widget']['format'] )
 
