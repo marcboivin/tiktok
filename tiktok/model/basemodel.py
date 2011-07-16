d9c7382d684a1db1d91cc86020fdc2ab14c88bd6
Gregory Eric Sanderson <gzou2000@gmail.com>
2011-07-16 14:32:09 -0400

    BaseModel now uses new helpers

--- tiktok/model/basemodel.py
+++ tiktok/model/basemodel.py
@@ -1,29 +1,35 @@
-
-utils = {}
-
-def get_utils():
-    global utils
-    return utils
-
-def set_utils( new_utils ):
-    global utils
-    utils = new_utils
+from tiktok.lib.helpers import get_helper
+from collections import namedtuple
 
 def resourcemethod( func ):
 
     @classmethod
     def wrapper( cls, *args, **kwargs ):
-        new_kw = get_utils()
-        new_kw.update( kwargs )
-        return func( cls, *args, **new_kw )
+
+        helpers = get_helper()
+
+        Context = namedtuple('Context', ['resource', 'duration', 'datetime', 'task_properties'] )
+
+        context = Context(
+            helpers.resource,
+            helpers.duration,
+            helpers.datetime,
+            helpers.config['task_properties']
+        )
+
+        if 'context' in kwargs:
+            for key, value in kwargs['context'].items():
+                setattr( context, key, value )
+
+        return func( cls, context, *args, **kwargs )
 
     return wrapper
 
 class BaseModel( dict ):
 
-    def __init__(self, attributes, **kwargs ):
+    def __init__(self, attributes, context, **kwargs ):
 
         dict.__init__( self )
-        for key, value in kwargs.items():
+        for key, value in context._asdict().items():
             setattr( self, key, value )
         self.update( attributes )
