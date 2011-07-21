88153aa613aed1f07d583ae82826988bd6418d6e
Gregory Eric Sanderson <gzou2000@gmail.com>
2011-07-20 22:00:11 -0400

    added command for printing a task table from widgets

--- tiktok/commands/widget.py
+++ tiktok/commands/widget.py
@@ -15,3 +15,29 @@ def list( args, helpers ):
     for widget in Widget.list():
         helpers.pprint( widget, helpers.config['widget']['format'] )
 
+def tasktable( args, helpers ):
+
+    widgets = Widget.list()
+    lines = []
+    printer = helpers.printer
+    widget_fmt = helpers.config['widget']['format']
+    task_fmt = helpers.config['task']['format']
+
+    for widget in widgets:
+        lines.append( (
+            'widget',
+            widget['id'],
+            printer.format( widget, widget_fmt )
+        ) )
+
+        lines.extend( (
+            (   'task',
+                widget['id'],
+                task['task_num'],
+                printer.format( task, task_fmt )
+            )   for task in widget.tasks( widget['id'] )
+        ) )
+
+    for line in lines:
+        print "\t".join( ( unicode(x) for x in line ) )
+
