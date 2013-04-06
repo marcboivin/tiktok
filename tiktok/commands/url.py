177a31e5248a9877483c41bbd44a50f16387645c
Marc Boivin <marc.boivin@libeo.com>
2013-04-05 20:05:30 -0400

    Change the logic so the live command now prints the system

--- tiktok/commands/url.py
+++ tiktok/commands/url.py
@@ -1,13 +1,24 @@
+from urlparse import urlparse
+
 from tiktok.model.task import Task
-from tiktok.model.project import Project
+from tiktok.model.url import URL
 
 import pprint
 import datetime
 import sys
 
+
 def live( args, helper ):
-    pprint(u'You\'re going to live clock on a url')
+    pprint.pprint( 'You\'re going to live clock on a url' )
+    
+    url = urlparse( args['source_url'] )
+    # Single subdomain implied. Also implied is that this subdomain 
+    # identifies the parser to use
+    system = url.hostname.split( '.' )[0]
+    
+    pprint.pprint( url )
+    
     
 def clock( args, helper ):
-    pprint('You\'re goint to clock a past task')
+    pprint.pprint( 'You\'re goint to clock a past task' )
         
